9aaffeffb62eed9a831ad5381f640bb622571f22
Caroldoourado29 <85802214+Caroldoourado@users.noreply.github.com>
2024-01-10 08:28:04 -0300

    atualização

--- criacao_de_widgets/lib/home_page.dart
+++ criacao_de_widgets/lib/home_page.dart
@@ -1,15 +1,21 @@
-  import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-    @override
-    Widget build(BuildContext context) {
-      return Scaffold(
-        appBar: AppBar(
-
-        ),
-        body: Center(
-          child: Text('Fluterando'),
-        ),
-      );
-    }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      body: Center(
+        child: Text('Fluterando'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          print('Clicou');
+        },
+      ),
+    );
   }
+}
