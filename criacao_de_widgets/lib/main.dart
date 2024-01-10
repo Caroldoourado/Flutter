4feb29db2cf4b42235ced91c0a4a82f392983cde
Caroldoourado29 <85802214+Caroldoourado@users.noreply.github.com>
2024-01-10 08:22:04 -0300

    atualização

--- criacao_de_widgets/lib/main.dart
+++ criacao_de_widgets/lib/main.dart
@@ -1,29 +1,6 @@
+import 'package:criacao_de_widgets/myapp.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
 }
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: HomePage(),
-    );
-  }
-  }
-
-  class HomePage extends StatelessWidget {
-    @override
-    Widget build(BuildContext context) {
-      return Scaffold(
-        appBar: AppBar(
-
-        ),
-        body: Center(
-          child: Text('Fluterando'),
-        ),
-      );
-    }
-  }
-
